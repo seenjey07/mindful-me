c5753e0e24c885f1f87304a400eb2b88684c2b94
seenjey07 <chrosales07@gmail.com>
2024-08-03 12:13:43 +0800

    Modified upon migration.

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,9 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_24_145550) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_03_040547) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "journals", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.date "date"
+    t.text "entry"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_journals_on_user_id"
+  end
+
+  create_table "moods", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.date "date"
+    t.string "mood"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_moods_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +44,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_24_145550) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "journals", "users"
+  add_foreign_key "moods", "users"
 end
