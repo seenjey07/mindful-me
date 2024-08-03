c7b73d4cdd3649b486005c83769cc08d8710ef2b
seenjey07 <chrosales07@gmail.com>
2024-08-03 15:38:55 +0800

    Setup moods controller for create, update, destroy, index, and mood_params.

--- backend/app/controllers/moods_controller.rb
+++ backend/app/controllers/moods_controller.rb
@@ -1,2 +1,33 @@
 class MoodsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    mood = current_user.moods.create(mood_params)
+    render json: mood
+  end
+
+  def update
+    mood = current_user.moods.find(params[:id])
+    mood.update(mood_params)
+    render json: mood
+  end
+
+  def destroy
+    mood = current_user.moods.find(params[:id])
+    mood.destroy
+    head :no_content
+  end
+
+  def index 
+    moods = current_user.moods
+    render json: moods
+  end
+
+
+  private
+
+  def mood_params
+    params.require(:mood).permit(:date, :mood)
+  end
+
 end
