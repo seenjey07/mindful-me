d01cfe6a694ca107b2fb9d320adbb5799689218b
Christine Rosales <chrosales07@gmail.com>
2024-08-03 17:23:35 +0800

    Changed text to sm

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <>
       <footer className="footer bg-neutral text-neutral-content p-3 absolute bottom-0">
-        <p className="text-white font-semibold justify-self-center">
+        <p className="text-white font-semibold justify-self-center text-xs">
           MindfulMe © {new Date().getFullYear()} - All right reserved
         </p>
       </footer>
