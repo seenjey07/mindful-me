dfdf1294af06e42bd10ceb8d21e97cf961b2c2e5
seenjey07 <chrosales07@gmail.com>
2024-08-03 12:12:28 +0800

    Setup the users controller for show,update,destroy, and user_params.

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,10 +1,25 @@
 class UsersController < ApplicationController
+before_action :authenticate_user! 
+
   def show
+    render json: current_user
   end
 
   def update
+    if current_user.update(user_params)
+      render json: current_user
+    else
+      render json: current_user.errors, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    current_user.destroy
+    head :no_content 
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :current_password)
 end
