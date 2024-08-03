fa1ab19f2580bf1e907705017a9b61551cbcb295
seenjey07 <chrosales07@gmail.com>
2024-08-03 15:54:24 +0800

    Added has_many for journals and moods.

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :jwt_authenticatable,
          jwt_revocation_strategy: Devise::JWT::RevocationStrategies::Null
+
+  has_many :moods 
+  has_many :journals
 end
