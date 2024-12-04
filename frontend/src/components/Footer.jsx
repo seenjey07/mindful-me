c5b741c3eb301f06f8b0398bb69420a5aca4b5fe
Christine Rosales <chrosales07@gmail.com>
2024-12-04 17:14:55 +0800

    Set position to fixed.

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,9 @@
 const Footer = () => {
   return (
     <>
-      <footer className="footer bg-neutral text-neutral-content p-3 absolute bottom-0">
+      <footer className="footer bg-neutral text-neutral-content p-3 fixed bottom-0 left-0 right-0">
         <p className="text-white font-semibold justify-self-center text-xs">
-          MindfulMe © {new Date().getFullYear()} - All right reserved
+          MindfulMe © {new Date().getFullYear()} - All rights reserved
         </p>
       </footer>
     </>
