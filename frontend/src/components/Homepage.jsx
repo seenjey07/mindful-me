c65099806f7b73f5068f18e66bc9a18216ed047c
Christine Rosales <chrosales07@gmail.com>
2024-12-04 17:16:04 +0800

    Removed the video toggle, created a separate file for it then called in the homepage component.

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -1,33 +1,12 @@
-import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import MindfulHer from "../assets/video-images/MindfulMe-Her.mp4";
-import MindfulHim from "../assets/video-images/MindfulMe-Him.mp4";
+import AppVideos from "./AppVideos";
 import Footer from "./Footer";
 
 const Homepage = () => {
-  const [currentVideo, setCurrentVideo] = useState("");
-
-  useEffect(() => {
-    const randomNumber = Math.random() < 0.5 ? 0 : 1;
-    setCurrentVideo(randomNumber === 0 ? MindfulHer : MindfulHim);
-  }, []);
-
   return (
     <>
       <div>
-        <div className="hero pt-12 pb-8 w-full">
-          <video
-            src={currentVideo}
-            type="video/mp4"
-            playsInline
-            loop
-            controls={false}
-            autoPlay
-            muted
-            className="w-40 h-40 md:w-52 md:h-52 rounded-full"
-          ></video>
-        </div>
-
+        <AppVideos />
         <div className="divider"></div>
 
         <div className="hero-overlay bg-transparent flex flex-col gap-0 w-full p-2">
@@ -41,18 +20,18 @@ const Homepage = () => {
             </em>
           </p>
         </div>
-      </div>
 
-      <div className="flex flex-col md:flex-row justify-center mt-8 gap-4">
-        <button className="btn btn-md px-10 mx-10 btn-warning">
-          <Link to="/login">Login</Link>
-        </button>
-        <button className="btn btn-md px-8 mx-10 btn-error">
-          <Link to="/register">Register</Link>
-        </button>
-      </div>
+        <div className="flex flex-col md:flex-row justify-center mt-8 gap-4">
+          <button className="btn btn-md px-10 mx-10 btn-warning">
+            <Link to="/login">Login</Link>
+          </button>
+          <button className="btn btn-md px-8 mx-10 btn-error">
+            <Link to="/register">Register</Link>
+          </button>
+        </div>
 
-      <div className="divider"></div>
+        <div className="divider"></div>
+      </div>
 
       <Footer />
     </>
